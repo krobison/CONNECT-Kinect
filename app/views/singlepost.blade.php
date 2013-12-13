e91dd5110a1c6507593632c3b8d6a94cb3c87acd
Gonzalo Gomez <ggomez@toilers.Mines.EDU>
2013-12-13 12:39:09 -0700

    more files

---
+++ app/views/singlepost.blade.php
@@ -0,0 +1,25 @@
+@extends('common.master')
+
+@section('content')
+	<h1>Hello world</h1>
+	{{View::make('common.newsfeedPost')->with('post', $post)}}
+
+	<div class="well">
+		@foreach ($post->comments as $comment)
+			<p>{{$comment->content}}</p>
+			<p>Posted by {{$comment->user->first}} {{$comment->user->last}} at {{$comment->created_at}}</p>
+		@endforeach
+	</div>
+
+	<div class="well">
+		{{ Form::open(array('url' => 'createComment', 'method'=>'post')) }}
+
+		{{ Form::textarea('content', 'hello world') }}
+
+		{{ Form::hidden('user_id', $user->id) }}
+
+		{{ Form::hidden('post_id', $post->id) }}
+
+		{{ Form::submit('Comment', array('class' => 'btn btn-lg btn-primary btn-block')) }}
+	</div>
+@stop
