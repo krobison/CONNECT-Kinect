b50e33d630d95bdd41de407c1c89086a911db809
krobison <krobison@mines.edu>
2013-12-18 13:56:18 -0700

    Display for taught courses

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -9,9 +9,33 @@ class DashboardController extends BaseController {
 	}
 	
 	public function showProfile($id) {
+		$studentClasses = "";
+		$studentTable = DB::table('course_user')
+		 	->where('user_id','=',$id)
+		 	->where('instructor','=','0');
+			$course_ids = $studentTable->lists('course_id');
+		if (!empty($course_ids)){
+			$studentClasses = DB::table('courses')
+				->whereIn('id',$course_ids)
+				->get();
+		}
+
+		$teacherClasses = "";
+		$teacherTable = DB::table('course_user')
+		 	->where('user_id','=',$id)
+		 	->where('instructor','=','1');
+			$course_ids = $teacherTable->lists('course_id');
+		if (!empty($course_ids)){
+			$teacherClasses = DB::table('courses')
+				->whereIn('id',$course_ids)
+				->get();
+		}
+
 		return View::make('profile')
 			->with('user', Auth::user())
-			->with('currentuser', User::find($id));
+			->with('currentuser', User::find($id))
+			->with('studentClasses',$studentClasses)
+			->with('teacherClasses',$teacherClasses);
 	}
 	
 	public function showHelpCenter() {
