047e8f914bd70d380dbfbc192e9d15fa0c75e8a7
Peter Choi <phkchoi89@gmail.com>
2013-12-18 15:38:59 -0700

    Cleaned up newsfeed post.

--- app/views/common/newsfeedPost.blade.php
+++ app/views/common/newsfeedPost.blade.php
@@ -17,20 +17,37 @@
 @endif
 
     <a href="{{URL::to('singlepost', $post->id)}}"> <p> {{ $post->content }} </p> </a>
-    <p> {{ $post->user->first }} {{ $post->user->last }}, {{ $post->created_at->diffForHumans() }} </p>
-    	
-    <p> Upvote count: {{ $post->postupvotes->count() }} </p>
-
-    {{ Form::open(array('url' => 'upvote', 'method'=>'post')) }}
+  
+    <div style="float:left; padding-right: 10px">
+	@if (!empty($post->user->picture))
+		@if ( File::exists('assets/img/profile_images/' . $post->user->picture ))
+			{{ HTML::image('assets/img/profile_images/'.$post->user->picture, '$comment->user->id', array('width' => '70', 'height' => '70')) }}
+		@else
+			{{ HTML::image('assets/img/dummy.png', $post->user->id , array('width' => '70', 'height' => '70')) }}
+		@endif
+	@else
+		{{ HTML::image('assets/img/dummy.png', $post->user->id , array('width' => '70', 'height' => '70')) }}
+	@endif
+	</div>
+	
+	{{ Form::open(array('url' => 'upvote', 'method'=>'post')) }}
 
     {{ Form::hidden('user_id', Auth::user()->id) }}
 
     {{ Form::hidden('post_id', $post->id) }}
 
     <button type="submit" class="btn btn-primary">
-        <i class="glyphicon glyphicon-hand-up"></i> Upvote
+        <i class="glyphicon glyphicon-hand-up"></i> Upvote - {{ $post->postupvotes->count() }}
     </button>
 
     {{ Form::close() }}
+    <br>
+
+	<p>{{ $post->user->first }} {{ $post->user->last }}, {{ $post->created_at->diffForHumans() }}</p>
+    
+    {{-- Need to clean up --}}
+    
+    
+    
 
 </div>
