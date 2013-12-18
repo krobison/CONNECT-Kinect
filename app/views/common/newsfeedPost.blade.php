a823f81a0472d705b7d5b953ba4a050343638e98
Thomas Brown <thbrown@mymail.mines.edu>
2013-12-18 13:50:53 -0700

    Split common post view into separate sections for each different type of post

--- app/views/common/newsfeedPost.blade.php
+++ app/views/common/newsfeedPost.blade.php
@@ -1,11 +1,35 @@
 <a href="{{URL::to('singlepost', $post->id)}}">
 
-	<div class="well">
-		<p> {{ $post->content }} </p>
-		<p> Posted by {{ $post->user->first }} {{ $post->user->last }} at {{ $post->created_at }} </p>
+	@if ($post->postable_type == "PostHelpRequest")
+		<div class="well">
+			<p> {{ $post->content }} </p>
+			<p> Posted by {{ $post->user->first }} {{ $post->user->last }} at {{ $post->created_at }} </p>
 
-		<a href="{{Request::url()}}">
-			{{ HTML::image('assets/img/upvote.png', 'upvote', array('width' => '22', 'height' => '22')) }}
-		</a>
-	</div>
+			<a href="{{Request::url()}}">
+				{{ HTML::image('assets/img/upvote.png', 'upvote', array('width' => '22', 'height' => '22')) }}
+			</a>
+			Langauge: {{$post->postable->language}}
+		</div>
+
+	@elseif ($post->postable_type == "PostHelpOffer")
+		<div class="well">
+			<p> {{ $post->content }} </p>
+			<p> Posted by {{ $post->user->first }} {{ $post->user->first }} {{ $post->user->last }} at {{ $post->created_at }} </p>
+
+			<a href="{{Request::url()}}">
+				{{ HTML::image('assets/img/upvote.png', 'upvote', array('width' => '22', 'height' => '22')) }}
+			</a>
+		</div>
+	@else
+		<div class="well">
+			<p> {{ $post->content }} </p>
+			<p> Posted by {{ $post->user->first }} {{ $post->user->first }} {{ $post->user->first }} {{ $post->user->last }} at {{ $post->created_at }} </p>
+
+			<a href="{{Request::url()}}">
+				{{ HTML::image('assets/img/upvote.png', 'upvote', array('width' => '22', 'height' => '22')) }}
+			</a>
+		</div>
+	@endif
+	
+
 </a>
