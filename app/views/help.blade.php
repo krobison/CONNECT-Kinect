1e81155c3e06e7510286806a6377c11afeb69aa1
Thomas Brown <thbrown@mymail.mines.edu>
2013-12-18 00:45:38 -0700

    Moved help center content into separate bootstrap panels

--- app/views/help.blade.php
+++ app/views/help.blade.php
@@ -20,25 +20,27 @@
 	#code-title:hover {
 		background-color: #F5F5F5;
 	}
+	.five-marg {
+		margin: 5px;
+	}
 	</style>
 @stop
 
 @section('content')
 	<h2>Help Center</h2>
-
-	<hr>
 	
-	<h4>New Help Post<h4>
 	<div id="new-post" class="panel panel-default">
 	    <div class="panel-heading">
-			<div class="btn-toolbar" role="toolbar">
-				<div class="btn-group">
-					<button id="need-help-button" type="button" class="btn btn-default">I Need Help</button>
-					<button id="offer-help-button" type="button" class="btn btn-default">I Want To Offer My Help</button>
-				</div>
-				<div class="btn-group">
-					<button id="hide-help-button" type="button" class="btn btn-default">Hide</button>
-				</div>
+			<h4>New Help Post<h4>
+		</div>
+		
+		<div class="btn-toolbar five-marg" role="toolbar">
+			<div class="btn-group">
+				<button id="need-help-button" type="button" class="btn btn-default btn-sm">I Need Help</button>
+				<button id="offer-help-button" type="button" class="btn btn-default btn-sm">I Want To Offer My Help</button>
+			</div>
+			<div class="btn-group">
+				<button id="hide-help-button" type="button" class="btn btn-default btn-sm">Hide</button>
 			</div>
 		</div>
 	    
@@ -145,29 +147,39 @@
 		</div>  	
 	</div>
 
-	<h4>Recent Help Requests</h4>
+	<div id="recent-help-requests" class="panel panel-default">
+		<div class="panel-heading">
+			<h4>Recent Help Requests</h4>
+		</div>
+		<div id="help-request" class="panel-body">
+			{{-- $post_counter = 0; --}}
+			@foreach (Post::where('postable_type', '=', 'PostHelpRequest')->take(5)->orderBy('id', 'DESC')->get() as $post)
+				{{ View::make('common.newsfeedPost')->with('post', $post) }}
+				{{-- $post_counter = $post_counter + 1 --}}
+			@endforeach
+			
+			{{-- @if( $post_counter >= 5 ) --}}
+				<button type="button" class="btn btn-default">Load more...</button>
+			{{-- @endif --}}
+		</div>
+	</div>
 	
-	{{-- $post_counter = 0; --}}
-	@foreach (Post::where('postable_type', '=', 'PostHelpRequest')->take(5)->orderBy('id', 'DESC')->get() as $post)
-		{{ View::make('common.newsfeedPost')->with('post', $post) }}
-		{{-- $post_counter = $post_counter + 1 --}}
-	@endforeach
-	
-	{{-- @if( $post_counter >= 5 ) --}}
-		<button type="button" class="btn btn-default">Load more...</button>
-	{{-- @endif --}}
-	
-	<h4>Recent Help Offers</h4>
-	
-	{{-- @$post_counter = 0; --}}
-	@foreach (Post::where('postable_type', '=', 'PostHelpOffer')->take(5)->orderBy('id', 'DESC')->get() as $post)
-		{{ View::make('common.newsfeedPost')->with('post', $post) }}
-		{{-- $post_counter = $post_counter + 1 --}}
-	@endforeach
-	
-	{{--@if( $post_counter >= 5 )--}}
-		<button type="button" class="btn btn-default">Load more...</button>
-	{{--@endif--}}
+	<div id="recent-help-offers" class="panel panel-default">
+		<div class="panel-heading">
+			<h4>Recent Help Offers</h4>
+		</div>
+		<div id="help-request" class="panel-body">
+			{{-- @$post_counter = 0; --}}
+			@foreach (Post::where('postable_type', '=', 'PostHelpOffer')->take(5)->orderBy('id', 'DESC')->get() as $post)
+				{{ View::make('common.newsfeedPost')->with('post', $post) }}
+				{{-- $post_counter = $post_counter + 1 --}}
+			@endforeach
+			
+			{{--@if( $post_counter >= 5 )--}}
+				<button type="button" class="btn btn-default">Load more...</button>
+			{{--@endif--}}
+		</div>
+	</div>
 	
 	<!-- Loading all scripts at the end for performance-->
 	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
