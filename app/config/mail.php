d7d11b073ef38867c672c858817ed527672cdbca
krobison <krobison@mines.edu>
2013-12-22 19:24:31 -0700

    Configured some email settings to avoid error, but smtp server must be configured to send email and test more

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'frodoBaggins@MiddleEarth.com', 'name' => 'Frodo Baggins'),
 
 	/*
 	|--------------------------------------------------------------------------
