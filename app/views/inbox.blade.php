700e34a25c80be7c4ff8c68bc9153d0c00e6cedc
krobison <krobison@mines.edu>
2013-12-27 01:07:48 -0700

    Fixed issue that broke messages, oops

--- app/views/inbox.blade.php
+++ app/views/inbox.blade.php
@@ -3,29 +3,25 @@
 @section('content')
 	<div id="centered">
 		<div>
-			@if ($users[0] != "")
-				@foreach ($messages as $message)
-					<div class="well">
-						<div>
-							<strong>To: </strong>
-							{{ $users[$message->to] }}
-							<br/>
-							<strong>From: </strong>
-							{{ $users[$message->from] }}
-							<br/>
-							<strong>Subject:</strong>
-							{{ $message->subject }}
-							<br/>
-						</div>
+			@foreach ($messages as $message)
+				<div class="well">
+					<div>
+						<strong>To: </strong>
+						{{ $users[$message->to] }}
 						<br/>
-						<div class="well">
-							{{ $message->content }}
-						</div>
+						<strong>From: </strong>
+						{{ $users[$message->from] }}
+						<br/>
+						<strong>Subject:</strong>
+						{{ $message->subject }}
+						<br/>
 					</div>
-				@endforeach
-			@else
-				<h2>You have no messages!</h2>
-			@endif
+					<br/>
+					<div class="well">
+						{{ $message->content }}
+					</div>
+				</div>
+			@endforeach
 		</div>
 	<div>
 @stop
