86d0093b451d551c8aae995a093d263383c5c48a
James Tyler Lyons <jlyons@toilers.Mines.EDU>
2013-12-27 19:48:06 -0700

    added bio searching to search page

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -10,25 +10,32 @@ class SearchController extends BaseController {
 	public function processSearch() {
 		$name = mysql_real_escape_string(Input::get('name'));
 		$courses = Input::get('classes');
-		foreach ($courses as $course) {
-			$course = mysql_real_escape_string($course);
-		}
-		$results = "";
+		$nameresults = NULL;
+		$bioresults = NULL;
 		$searchCourses = "";
 		
 		if (empty($courses) && empty($name)) {
 			return View::make('search')->with('user', Auth::user());
 		
 		} elseif(empty($courses)) {
-			$results = DB::select(
+			$nameresults = DB::select(
 			"SELECT *
 			FROM users
 			WHERE CONCAT(first, ' ', last) LIKE '%$name%' OR last LIKE '%$name%'"
 			);
 			
-		} else {
+			$bioresults = DB::select(
+			"SELECT *
+			FROM users
+			WHERE bio LIKE '%$name%'"
+			);
+			
+		} elseif(empty($name)) {
+			foreach ($courses as $course) {
+				$course = mysql_real_escape_string($course);
+			}
 			$courseIds = implode(",", $courses);
-			$results = DB::select(
+			$nameresults = DB::select(
 			"SELECT u.* 
 			FROM users u
 			JOIN course_user l ON u.id = l.user_id
@@ -43,10 +50,43 @@ class SearchController extends BaseController {
 			$searchCourses = DB::table('courses')
 				->whereIn('id',$courses)
 				->get();
+				
+		} else {
+			foreach ($courses as $course) {
+				$course = mysql_real_escape_string($course);
+			}
+			$courseIds = implode(",", $courses);
+			$nameresults = DB::select(
+			"SELECT u.* 
+			FROM users u
+			JOIN course_user l ON u.id = l.user_id
+			JOIN courses c ON c.id = l.course_id
+			WHERE FIND_IN_SET ( c.id, '$courseIds' )
+			AND ( CONCAT(u.first, ' ',u.last) LIKE '%$name%' 
+			OR last LIKE '%$name%')
+			GROUP BY u.id
+			ORDER BY COUNT(1) DESC"
+			);
+			
+			$bioresults = DB::select(
+			"SELECT u.* 
+			FROM users u
+			JOIN course_user l ON u.id = l.user_id
+			JOIN courses c ON c.id = l.course_id
+			WHERE FIND_IN_SET ( c.id, '$courseIds' )
+			AND bio LIKE '%$name%'
+			GROUP BY u.id
+			ORDER BY COUNT(1) DESC"
+			);
+			
+			$searchCourses = DB::table('courses')
+				->whereIn('id',$courses)
+				->get();
 		}
 		
 		return View::make('search')
-			->with('results', $results)
+			->with('nameresults', $nameresults)
+			->with('bioresults', $bioresults)
 			->with('searchCourses', $searchCourses)
 			->with('name', $name)
 			->with('user', Auth::user());
--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -33,8 +33,9 @@
 	
 	<br>
 	
-	@if(isset($results))
-		@foreach($results as $result) 
+	@if(isset($nameresults))
+	@if(!empty($nameresults))<h2>Names</h2>@endif
+		@foreach($nameresults as $result) 
 		<div class="well">
 			<div class="row">
 				<div class="picture">
@@ -83,6 +84,60 @@
 			
 		</div>
 		@endforeach
+	@endif
+	
+	@if(isset($bioresults))
+	@if(!empty($bioresults))<h2>Bio</h2>@endif
+	@foreach($bioresults as $result) 
+	<div class="well">
+		<div class="row">
+			<div class="picture">
+				<a href="{{URL::to('profile', $result->id)}}">
+				@if(is_null($result->picture))
+					{{ HTML::image('assets/img/dummy.png', 'profile picture', array('width' => '128', 'height' => '128')) }}
+				@else
+					{{ HTML::image('assets/img/profile_images/'.$result->picture, 'profile picture', array('width' => '128', 'height' => '128')) }}
+				@endif
+				</a>
+			</div>
+			<span>
+				<h3><a href="{{URL::to('profile', $result->id)}}">{{{ $result->first }}} {{{ $result->last }}} </a></h3>
+			</span>
+		<span>
+
+			<div>
+
+				@foreach(User::find($result->id)->courses as $course)
+					@if (!empty($searchCourses))
+						@foreach($searchCourses as $searchCourse)
+							@if($course->id == $searchCourse->id)
+								<span class="courselabelmatch">{{{$course->prefix}}}{{{$course->number}}} - {{{$course->name}}}</span>
+							@endif
+						@endforeach 
+					@endif
+				@endforeach
+				@foreach(User::find($result->id)->courses as $course)
+					<?php $t = false ?>
+					@if (!empty($searchCourses))
+						@foreach($searchCourses as $searchCourse)
+							@if($course->id == $searchCourse->id)
+								<?php $t = true ?>
+							@endif
+						@endforeach 
+					@endif
+					@if(!$t)
+						<span class="courselabel">{{{$course->prefix}}}{{{$course->number}}} - {{{$course->name}}}</span>
+					@endif
+				@endforeach
+					
+					
+			</div>
+		</span>
+		{{{strip_tags($result->bio)}}}
+		</div>
+			
+	</div>
+	@endforeach
 	@endif
 	<!-- Loading all scripts at the end for performance-->
 	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
