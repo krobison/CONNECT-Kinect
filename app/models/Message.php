a012e288604bc6e1f2fbc191ff01fb7561c5c00e
Thomas Brown <thbrown@mymail.mines.edu>
2013-12-28 01:27:17 -0700

    Changed message models, probably broke some things. Still some work to be done on my part

--- app/models/Message.php
+++ app/models/Message.php
@@ -1,22 +1,10 @@
 <?php
 class Message extends Eloquent {
-	public function fromUser() {
-		return $this->belongsTo('User', 'from');
+	public function recipients() {
+		return $this->belongsToMany('User');
 	}
 
-	public function toUser() {
-		return $this->belongsTo('User', 'to');
-	}
-}
-
-class User_message extends Eloquent {
-
-	/**
-	 * Automatically uses users_messages table based on naming conventions
-	 */
-	protected $table = 'user_messages';
-
-	public function toUser() {
-		return $this->belongsTo('User', 'user_id');
+	public function sender() {
+		return User::find($this->from);
 	}
 }
--- app/views/messageCompose.blade.php
+++ app/views/messageCompose.blade.php
@@ -2,22 +2,25 @@
 
 @section('additionalHeaders')
 	{{ HTML::style('assets/css/select2.css') }}
-	{{ HTML::script('assets/js/select2.js') }}
 @stop
 
 @section('content')
+	@if(Session::has('message'))	
+		<p> {{Session::get('message');}} </p>
+	@endif
+
 	{{ Form::open(array('url' => 'messageCompose', 'method' => 'POST')) }}
-		{{ Form::label('from', 'From') }} {{ $user->first }} {{ $user->last }}
+		{{ Form::label('from', 'From') }} {{{ $user->first }}} {{{ $user->last }}}
 		<br/>
 
 		{{ Form::label('toUsers', 'Please select users to message') }}
 		<br/>
-		<select multiple class="select2-container" name="to[]">
+		<select multiple class="select2-container" style="width:80%" name="to[]">
 			@foreach(User::all() as $user)
 				@if($toUser != "none" && $user->id == $toUser->id)
-					<option value="{{ $user->id }}" selected>{{ $user->first }} {{ $user->last }}</option>
+					<option value="{{{ $user->id }}}" selected>{{{ $user->first }}} {{{ $user->last }}}</option>
 				@elseif ($user->id != Auth::user()->id)
-					<option value="{{ $user->id }}">{{ $user->first }} {{ $user->last }}</option>
+					<option value="{{{ $user->id }}}">{{{ $user->first }}} {{{ $user->last }}}</option>
 				@endif
 			@endforeach
 		</select>
@@ -30,4 +33,11 @@
 		{{ Form::submit('Send Message') }}
 		{{ Form::hidden('from', $user->id) }}
 	{{ Form::close() }}
+	
+	{{ HTML::script('assets/js/select2.min.js') }}
+	<script>
+		$(".select2-container").select2({
+			placeholder: "Select Message Recepient(s)"
+		});
+	</script>
 @stop
