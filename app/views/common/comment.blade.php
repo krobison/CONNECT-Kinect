ffa51dd36d3d17e6159a5f922b8b1f821e74f078
David Hunter <dhunter@mines.edu>
2013-12-30 13:54:23 -0700

    Added deleting to user comments, still working on editing

--- app/views/common/comment.blade.php
+++ app/views/common/comment.blade.php
@@ -1,13 +1,47 @@
+<script>
+	$(window).load(function() {
+		$("#save").hide();
+		$("#edit").click(function() { /* assign anonymous function to click event */
+    		alert($("#paragraph{{$comment->id}}").text());
+    		var p = $("#paragraph{{$comment->id}}"); /* store reference to <p> element */
+
+   			 /* get p.text() without the formatting */
+   			 var t = p.text().replace("\n", "").replace(/\s{2,}/g, " ").trim();
+
+    		/* create new textarea element with additional attributes */
+    		var ta = $("<textarea/>", {
+        		"class": "edit",
+        		"id": "editbox",
+        		"text": t,
+        		"css": {
+            		"width": p.css('width')
+        		}
+    		});
+
+    		$("#editbox").ready(function() {
+    			$("#editbox").keyup(function() {
+  					alert( "Handler for .keypress() called." );
+				});
+    		});
+
+    		p.replaceWith(ta); /* replace p with ta */
+    		$("#save").show();
+    		$("#edit").hide();
+    	});
+	});
+</script>
+
 <div class="well">
 	<div style="float:left; padding-right: 10px">
 		{{HTML::image($comment->user->getProfilePictureURL(), '$comment->user->id', array('width' => '70', 'height' => '70', 'class' => 'img-circle'))}}
 	</div>
 	
-	<p>{{{ $comment->content }}}</p>
-	<p>{{{ $comment->user->first }}} {{{ $comment->user->last }}}, {{{ $comment->created_at->diffForHumans() }}}</p>
+	<div>
+		<p id="paragraph{{$comment->id}}">{{{ $comment->content }}}</p>
+	</div>
 
 	@if (!empty($comment->code))
-		<br>
+		<div>
 			Language: {{ $comment->language }}
 			<div id="editor{{$comment->id}}">
 				{{{ $comment->code }}}
@@ -26,6 +60,30 @@
 				maxLines: 50
 			});
 		</script>
+		</div>
+	@endif
+
+	<p>{{{ $comment->user->first }}} {{{ $comment->user->last }}}, {{{ $comment->created_at->diffForHumans() }}}</p>
+
+	@if(Auth::user()->id == $comment->user_id)
+		<button type="submit" class="btn btn-primary" style="float:left;" id="edit">
+				<span class="glyphicon glyphicon-pencil"></span> Edit Comment
+		</button>
+
+		{{ Form::open(array('url' => 'saveeditcomment', 'method'=>'post')) }}
+		{{ Form::hidden('id', $comment->id) }}
+		{{ Form::hidden('toSave') }}
+		<button type="submit" id="save" class="btn btn-primary" style="float:left;">
+				<span class="glyphicon glyphicon-floppy-saved"></span> Save
+		</button>
+		{{ Form::close() }}
+
+		{{ Form::open(array('url' => 'deleteusercomment', 'method'=>'post')) }}
+		{{ Form::hidden('id', $comment->id) }}
+		<button type="submit" class="btn btn-danger" style="float:right;" onclick="return confirm('Are you sure you would like to delete this comment FOREVER?');">
+				<span class="glyphicon glyphicon-trash"></span> Delete Comment
+		</button>
+		{{ Form::close() }}
 	@endif
 	
 	@if(Auth::user()->admin == '1')
