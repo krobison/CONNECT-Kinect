5db376ae5b90d2e897227551fa68c660fb001972
James Tyler Lyons <jlyons@toilers.Mines.EDU>
2013-12-30 15:08:26 -0700

    updated cs projects page to display project posts

--- app/views/projects.blade.php
+++ app/views/projects.blade.php
@@ -77,11 +77,16 @@
 		</div> 
 	</div>
 	
+	@if($user->admin == '1')
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h4>Projects That Need Approval</h4>
+			<h4>Projects That Need Approval
+			<div class="btn-group" id="new-post-buttons">
+				<button id="hide-approve-projects-button" type="button" class="btn btn-default btn-sm">Hide</button>
+			</div>
+			</h4>
 		</div>
-		<div id="help-request" class="panel-body">
+		<div id="approve-projects" class="panel-body">
 			{{-- $post_counter = 0; --}}
 			@foreach (Post::where('postable_type', '=', 'PostProject')->orderBy('id', 'DESC')->get() as $post)
 				@if($post->postable->approved == '0')
@@ -95,6 +100,27 @@
 			{{-- @endif --}}
 		</div>
 	</div>
+	@endif
+	
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h4>Recent CS Projects</h4>
+		</div>
+		<div id="help-request" class="panel-body">
+			{{-- $post_counter = 0; --}}
+			@foreach (Post::where('postable_type', '=', 'PostProject')->orderBy('id', 'DESC')->get() as $post)
+				@if($post->postable->approved == '1')
+					{{ View::make('common.newsfeedPost')->with('post', $post) }}
+					{{-- $post_counter = $post_counter + 1 --}}
+				@endif
+			@endforeach
+			
+			{{-- @if( $post_counter >= 5 ) --}}
+				<button type="button" class="btn btn-default">Load more... <i> Not Working </i> </button>
+			{{-- @endif --}}
+		</div>
+	</div>
+	
 	
 	<!-- Loading all scripts at the end for performance-->
 	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
@@ -103,6 +129,10 @@
 	// Hide and show post divs on button press
 		$('#hide-new-post-button').click(function() {
 			$('#new-post-body').toggle(200);
+		});
+		
+		$('#hide-approve-projects-button').click(function() {
+			$('#approve-projects').toggle(200);
 		});
 	</script>
 @stop
