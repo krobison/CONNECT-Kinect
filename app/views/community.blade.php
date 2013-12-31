fcda2f8795fb52d4257d7a87102dde4926c27ba1
Thomas Brown <thbrown@mymail.mines.edu>
2013-12-30 19:23:37 -0700

    Relaced Rick Astley with a graph of connect signups.

--- app/views/community.blade.php
+++ app/views/community.blade.php
@@ -4,15 +4,20 @@
 	<style type="text/css" media="screen">
 
 	</style>
+	{{ HTML::script('assets/js/d3.min.js') }}
 @stop
 
 @section('content')
 	<h2>Community Tool</h2>
 	<p>
+		<!-- This js must be imported before d3 script tags below-->
 		This page is a holder for the community tool, this was a tool in some previous connect versions that let people see other's interest areas. We need to integrate the tool in to CS CONNECT and base it around hashtags. So hashtags need to be done first, I'm working on it. -- Thomas
+		<script type="text/javascript">
+            d3.select("body").append("p").text("New paragraph!");
+        </script>
 	</p>
 	
 	<!-- Loading all scripts at the end for performance-->
 	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
-	
+
 @stop
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -7,6 +7,47 @@
 	{{ HTML::style('assets/css/signin.css') }}
 	{{ HTML::style('assets/css/social-buttons.css') }}
 	{{ HTML::style('assets/css/font-awesome.min.css') }}
+	
+	{{ HTML::script('assets/js/d3.min.js') }}
+	
+	<style>
+		/* tell the SVG path to be a thin blue line without any area fill */
+		path {
+			stroke: steelblue;
+			stroke-width: 1;
+			fill: none;
+		}
+
+		.axis {
+		  shape-rendering: crispEdges;
+		}
+
+		.x.axis line {
+		  stroke: lightgrey;
+		}
+
+		.x.axis .minor {
+		  stroke-opacity: .5;
+		}
+
+		.x.axis path {
+		  display: none;
+		}
+
+		.y.axis line, .y.axis path {
+		  fill: none;
+		  stroke: #000;
+		}
+		
+		.x.label {
+			text: "asljdkas";
+		}
+		
+		.y.label {
+			text: "asljdkas";
+		}
+	</style>
+		
 </head>
 
 <body>
@@ -20,25 +61,96 @@
 		@endif
 		</div>	
 		<div id="rightPanel">
-			<div class="video-container"><iframe width="560" height="315" src="//www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe></div>
+		
+			<h3> CS Connect Accounts </h3>
+			Time: {{{date('Y-m-d H:i:s',time() - 60*60*24*7*2)}}} to Time: {{{date('Y-m-d H:i:s')}}}
+			<br>
+
+			<div id="graph" class="aGraph" ></div>
+			<script>
+				// define dimensions of graph
+				var m = [80, 80, 80, 80]; // margins
+				var w = 600 - m[1] - m[3]; // width
+				var h = 400 - m[0] - m[2]; // height
+				
+				// create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+				// This does 14 separate db queries, this needs to be optimized later
+				var data = [];
+				@for ($i = 60*60*24*7*2; $i > 0; $i -= 60*60*24)
+					data.push({{{ User::where('created_at', '<=', date('Y-m-d H:i:s',time() - $i))->count() }}});
+				@endfor
+
+				//Users::where('timestamp', '>=', time() - (24*60*60))->count();
+
+				// display all values
+				// X scale will fit all values from data[] within pixels 0-w
+				var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+				// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+				var y = d3.scale.linear().domain([0, data[data.length-1]]).range([h, 0]);
+					// automatically determining max range can work something like this
+					// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+
+				// create a line function that can convert data[] into x and y points
+				var line = d3.svg.line()
+					// assign the X function to plot our line as we wish
+					.x(function(d,i) { 
+						// return the X coordinate where we want to plot this datapoint
+						return x(i); 
+					})
+					.y(function(d) { 
+						// return the Y coordinate where we want to plot this datapoint
+						return y(d); 
+					})
+
+				// Add an SVG element with the desired dimensions and margin.
+				var graph = d3.select("#graph").append("svg:svg")
+					  .attr("width", w + m[1] + m[3])
+					  .attr("height", h + m[0] + m[2])
+					  .append("svg:g")
+					  .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+
+				// create yAxis
+				var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+				// Add the x-axis.
+				graph.append("svg:g")
+					  .attr("class", "x axis")
+					  .attr("transform", "translate(0," + h + ")")
+					  .call(xAxis);
+
+
+				// create left yAxis
+				var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+				// Add the y-axis to the left
+				graph.append("svg:g")
+					  .attr("class", "y axis")
+					  .attr("transform", "translate(-25,0)")
+					  .call(yAxisLeft);
+				
+				// Add the line by appending an svg:path element with the data line we created above
+				// do this AFTER the axes above so that the line is above the tick-lines
+				graph.append("svg:path").attr("d", line(data));
+				//<div class="video-container"><iframe width="560" height="315" src="//www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe></div>
+			</script>
+		
+			
 		</div>
 		<div id="leftPanel">
 			
 			{{ Form::open(array('url' => 'loginuser','class' => 'form-signin')) }}
 				
-					<h2 class="form-signin-heading" id="welcome">Welcome to CS CONNECT!</h2>
-					
-					{{ Form::text('email', '', array('class' => 'form-control','placeholder' => 'Email','autofocus' => 'true')) }}
-					{{ Form::password('password', array('class' => 'form-control','placeholder' => 'Password')) }}
-														
-					<label class="checkbox" style="text-align:left;">
-						<input type="checkbox" value="remember-me"> Remember me <i> Not Working </i>
-					</label>
-					<div style="text-align:left;"><a href="{{asset('password/reset')}}"><i>I forgot my password</i></a><br><br></div>
-					
-					<div class="button-div">
-						{{ Form::submit('Sign in', array('class' => 'btn btn-lg btn-primary btn-block')) }}
-					</div>
+				<h2 class="form-signin-heading" id="welcome">Welcome to CS CONNECT!</h2>
+				
+				{{ Form::text('email', '', array('class' => 'form-control','placeholder' => 'Email','autofocus' => 'true')) }}
+				{{ Form::password('password', array('class' => 'form-control','placeholder' => 'Password')) }}
+													
+				<label class="checkbox" style="text-align:left;">
+					<input type="checkbox" value="remember-me"> Remember me <i> Not Working </i>
+				</label>
+				<div style="text-align:left;"><a href="{{asset('password/reset')}}"><i>I forgot my password</i></a><br><br></div>
+				
+				<div class="button-div">
+					{{ Form::submit('Sign in', array('class' => 'btn btn-lg btn-primary btn-block')) }}
+				</div>
 					
 			{{ Form::close() }}
 				
