33ef99d5292e6b77d7803a508e7729799b2db273
Thomas Brown <thbrown@mymail.mines.edu>
2014-01-04 10:27:00 -0700

    Added user tags to the edit profile page

--- app/views/editProfile.blade.php
+++ app/views/editProfile.blade.php
@@ -7,13 +7,19 @@
 
 @section('content')
 	<div class="basic">
+	
+		{{-- Delete Account Button --}}
 		<form class="form-horizontal" role="form" action="{{ URL::to('deleteaccount/')}}" method="post">
 			<button type="submit" class="btn btn-danger" style="float:right;margin-top:16px;" onclick="return confirm('Are you sure you would like to delete your account FOREVER? This includes deletion of all posts, comments, and up-votes as well as uploaded projects and pictures.');">
 				<span class="glyphicon glyphicon-trash"></span> Delete Account
 			</button>
 		</form>
+		
 		<h3 class="editheader">Edit Profile <small>{{$user->first.' '.$user->last.' ('.$user->email.')'}}</small></h3>
+		
 		{{ Form::open(array('url' => 'changedAccount', 'files' => true, 'class' => 'form-horizontal')) }}
+		
+			{{-- Profile Picture --}}
 			@if(is_null(Auth::User()->picture))
 				{{ HTML::image('assets/img/dummy.png', 'profile picture', array('width' => '256', 'height' => '256', 'class' => 'editimage')) }}
 			@else
@@ -21,16 +27,19 @@
 			@endif 
 			<div class="form-group">
 				<label for="first" class="col-sm-3 control-label">Profile Picture</label>
-					<div class="col-sm-4">
-						{{Form::file('profilepic', array())}}	
-					</div>
+				<div class="col-sm-4">
+					{{Form::file('profilepic', array())}}	
+				</div>
 			</div>
 			@if($errors->has('profilepic'))
 				<span class="errormessage">
 					{{$errors->first('profilepic')}}
 				</span>
 			@endif
+			
 			<hr>
+			
+			{{-- First Name --}}
 			@if($errors->has('first'))
 				<span class="errormessage">
 					{{$errors->first('first')}}
@@ -38,10 +47,12 @@
 			@endif
 			<div class="form-group">
 				<label for="first" class="col-sm-3 control-label">First</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" id="first" name="first" value="{{{$user->first}}}"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" id="first" name="first" value="{{{$user->first}}}"/>
+				</div>
 			</div>
+			
+			{{-- Last Name --}}
 			@if($errors->has('last'))
 				<span class="errormessage">
 					{{$errors->first('last')}}
@@ -53,101 +64,133 @@
 						<input type="text" class="form-control" id="last" name="last" value="{{{$user->last}}}"/>
 					</div>
 			</div>
+			
+			{{-- Degree --}}
 			<div class="form-group">
 				<label for="degree" class="col-sm-3 control-label">Degree</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" id="degree" name="degree" value="{{{$user->degree_type}}}"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" id="degree" name="degree" value="{{{$user->degree_type}}}"/>
+				</div>
 			</div>
+			
+			{{-- Graduation Date --}}
 			<div class="form-group">
 				<label for="grad" class="col-sm-3 control-label">Graduation</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" id="grad" name="grad" value="{{{$user->grad_date}}}"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" id="grad" name="grad" value="{{{$user->grad_date}}}"/>
+				</div>
 			</div>
+			
+			{{-- Major --}}
 			<div class="form-group">
 				<label for="major" class="col-sm-3 control-label">Major</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" id="major" name="major" value="{{{$user->major}}}"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" id="major" name="major" value="{{{$user->major}}}"/>
+				</div>
 			</div>
+			
+			{{-- Minor --}}
 			<div class="form-group">
 				<label for="minor" class="col-sm-3 control-label">Minor</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" id="minor" name="minor" value="{{{$user->minor}}}"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="text" class="form-control" id="minor" name="minor" value="{{{$user->minor}}}"/>
+				</div>
 			</div>
+			
 			<hr>
-				@if (isset($badPassword))
-					<span class="errormessage">
-						You have entered your current password incorrectly
-					</span>
-				@endif
-				<div class="form-group">
+			
+			{{-- Password --}}
+			@if (isset($badPassword))
+				<span class="errormessage">
+					You have entered your current password incorrectly
+				</span>
+			@endif
+			<div class="form-group">
 				<label for="old" class="col-sm-3 control-label">Current Password</label>
-					<div class="col-sm-4">
-						<input type="password" class="form-control" id="old" name="old" value=""/>
-					</div>
+				<div class="col-sm-4">
+					<input type="password" class="form-control" id="old" name="old" value=""/>
 				</div>
-				
-				@if($errors->has('new'))
-				<span class="errormessage">
-					{{$errors->first('new')}}
-				</span>
-				@endif
-				<div class="form-group">
+			</div>
+			@if($errors->has('new'))
+			<span class="errormessage">
+				{{$errors->first('new')}}
+			</span>
+			@endif
+			<div class="form-group">
 				<label for="last" class="col-sm-3 control-label">New Password</label>
-					<div class="col-sm-4">
-						<input type="password" class="form-control" id="new" name="new"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="password" class="form-control" id="new" name="new"/>
 				</div>
-				@if($errors->has('new_confirmation'))
-				<span class="errormessage">
-					{{$errors->first('new_confirmaiton')}}
-				</span>
-				@endif
-				<div class="form-group">
+			</div>
+			@if($errors->has('new_confirmation'))
+			<span class="errormessage">
+				{{$errors->first('new_confirmaiton')}}
+			</span>
+			@endif
+			<div class="form-group">
 				<label for="new_confirmation" class="col-sm-3 control-label">Confirm Password</label>
-					<div class="col-sm-4">
-						<input type="password" class="form-control" id="new_confirmation" name="new_confirmation"/>
-					</div>
+				<div class="col-sm-4">
+					<input type="password" class="form-control" id="new_confirmation" name="new_confirmation"/>
 				</div>
-				<hr>
-				
-				@if($errors->has('bio'))
-				<span class="errormessage">
-					{{$errors->first('bio')}}
-				</span>
-				@endif
-				<div class="form-group">
-				<label for="bio" class="col-sm-3 control-label">Bio</label>
-					<div class="col-sm-4">
-						<textarea type="text" class="form-control" id="bio" name="bio">{{{$user->bio}}}</textarea>
-					</div>
-				</div>
+			</div>
+			
 			<hr>
-				<div class="form-group">
+			
+			{{-- Bio --}}
+			@if($errors->has('bio'))
+			<span class="errormessage">
+				{{$errors->first('bio')}}
+			</span>
+			@endif
+			<div class="form-group">
+				<label for="bio" class="col-sm-3 control-label">Bio</label>
+				<div class="col-sm-4">
+					<textarea type="text" class="form-control" id="bio" name="bio">{{{$user->bio}}}</textarea>
+				</div>
+			</div>
+
+			<hr>
+			
+			{{-- Tags --}}
+			<div class="form-group">
+				<label for="hashtags[]" class="col-sm-3 control-label">Tags</label>
+				<div class="col-sm-4">
+					<select multiple class="select2-container-tags classSelect" name="hashtags[]" id="tagSelect">
+						@foreach($tagHTML as $html)
+							{{$html}}
+						@endforeach
+					</select>
+				</div>
+			</div>
+			
+			<hr>
+			
+			{{-- Classes Teaching --}}
+			<div class="form-group">
 				<label for="classesStudent[]" class="col-sm-3 control-label">Courses Taking</label>
-					<div class="col-sm-4">
-						<select multiple class="select2-container-student classSelect" name="classesStudent[]" id="studentSelect">
-							@foreach($studentSelectHTML as $html)
+				<div class="col-sm-4">
+					<select multiple class="select2-container-student classSelect" name="classesStudent[]" id="studentSelect">
+						@foreach($studentSelectHTML as $html)
+							{{$html}}
+						@endforeach
+					</select>
+				</div>
+			</div>
+			
+			{{-- Classes Taking --}}
+			<div class="form-group">
+				<label for="classesTeacher[]" class="col-sm-3 control-label">Courses Teaching</label>
+				<div class="col-sm-4">
+					<select multiple class="select2-container-teacher classSelect" name="classesTeacher[]" id="teacherSelect">
+						<optgroup label="Computer Science">
+							@foreach($teacherSelectHTML as $html)
 								{{$html}}
 							@endforeach
-						</select>
-					</div>
+						</optgroup>
+					</select>
 				</div>
-				<div class="form-group">
-				<label for="classesTeacher[]" class="col-sm-3 control-label">Courses Teaching</label>
-					<div class="col-sm-4">
-						<select multiple class="select2-container-teacher classSelect" name="classesTeacher[]" id="teacherSelect">
-							<optgroup label="Computer Science">
-								@foreach($teacherSelectHTML as $html)
-									{{$html}}
-								@endforeach
-							</optgroup>
-						</select>
-					</div>
-				</div>
+			</div>
+			
 			<br>
 			<button type="submit" class="btn btn-primary" style="float:right">
 				<span class="glyphicon glyphicon-ok"></span> Save Changes
@@ -160,7 +203,7 @@
 		</form>
 		<form action="">
 			<button type="submit" class="btn btn-warning" id="resetbutton">
-				<span class="glyphicon glyphicon-arrow-left"></span> Reset
+				<span class="glyphicon glyphicon-arrow-left"></span> Undo Any Changes
 			</button>
 		</form>
 	</div>
@@ -174,7 +217,10 @@
 			});
 			$(".select2-container-teacher").select2({
 				placeholder: "Select Your Classes"
-			});		
+			});
+			$(".select2-container-tags").select2({
+				placeholder: "Select Your Classes"
+			});	
 		});
 	</script>
 @stop
