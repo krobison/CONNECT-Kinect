6c52270daeaee875c0a8f0c8214d0eec82ad6eea
David Hunter <dhunter@mines.edu>
2014-01-05 16:57:20 -0700

    Fixed a bug in my javascript

--- app/views/javascript/comment.blade.php
+++ app/views/javascript/comment.blade.php
@@ -44,7 +44,7 @@
 						$("#save{{$comment->id}}").attr("disabled", false);
 						$('[name="toSaveNewCode{{$comment->id}}"]').attr('value', ace.edit("editor{{$comment->id}}").getValue().trim());
 					});
-					$('#language-select').on("change", function() {
+					$('#language-select{{$comment->id}}').on("change", function() {
 						$('[name="toSaveLanguage{{$comment->id}}"]').attr('value', $(".select2-chosen").html());
 					});
 	    		@endif
