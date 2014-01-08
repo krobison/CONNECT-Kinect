71ed7daf34bbdbb2d9bc7f48c0183ddc44e42dee
Connect <connect@mines.edu>
2014-01-08 16:29:51 -0700

    Fixing permissions script

--- fixPermissions.sh
+++ fixPermissions.sh
@@ -6,8 +6,11 @@ chmod o+w -R app/storage
 # Write permissions for HTML purifyer
 chmod o+w app/purify/HTMLPurifier/DefinitionCache/Serializer
 
-# Write permissions to upload cs prject files
+# Write permissions to upload cs project files
 chmod o+w assets/csproject_files
+
+# Write permissions to upload cs project images
+chmod o+w assets/img/csproject_images
 
 # Write permissions to save profile pictures
 chmod o+w assets/img/profile_images
