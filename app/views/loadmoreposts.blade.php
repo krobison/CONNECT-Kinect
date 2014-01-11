035586101f8cff998d05dd2710e991c497ae452d
Thomas Brown <thbrown@mymail.mines.edu>
2014-01-11 14:09:49 -0700

    Ordered Notifications

--- app/views/loadmoreposts.blade.php
+++ app/views/loadmoreposts.blade.php
@@ -1,13 +1,3 @@
-
-
-
-	{{ HTML::style('assets/css/bootstrap.min.css') }}
-	{{ HTML::script('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js') }}
-	{{ HTML::script('assets/js/jquery.stellar.min.js') }}
-	{{ HTML::script('assets/js/bootstrap.min.js') }}
-	{{ HTML::style('assets/css/select2.css') }}
-	{{ HTML::style('assets/css/posts.css') }}
-	
 	<!-- Generate all recent user posts -->
 	
 	@foreach ($posts as $postid)
