8ceeced9fe3b251cd65d33fb16808d6fcef6a754
David Hunter <dhunter@mines.edu>
2014-01-12 22:27:11 -0700

    Changed logging from being written to a file to database

--- app/controllers/CSConnectController.php
+++ app/controllers/CSConnectController.php
@@ -4,8 +4,12 @@ class CSConnectController extends BaseController {
 	
 	public function showCs_connect() {
 		$posts = Post::orderBy('created_at', 'DESC')->where('postable_type', '=', 'PostFeedback')->get();
-		$user_id['user_id'] = Auth::user()->id;
-		Log::info('cs_connect accessed', $user_id);
+
+		$log = new CustomLog;	
+		$log->user_id = Auth::user()->id;
+		$log->event_type = "cs_connect accessed";
+		$log->save();
+
 		return View::make('cs_connect')
 			->with('user', Auth::user())
 			->with('posts', $posts);
