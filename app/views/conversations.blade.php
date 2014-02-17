1c46573be3f4c43a0ef2734e208eecd86b4ae013
Thomas Brown <thbrown@mymail.mines.edu>
2014-02-17 14:55:56 -0700

    Conversation particants are now shown in the conversations view.

--- app/views/conversations.blade.php
+++ app/views/conversations.blade.php
@@ -22,14 +22,17 @@
 				@if(is_null(User::find($conversation->owner)->picture))
 					{{ HTML::image('assets/img/dummy.png', 'profile picture', array('width' => '70', 'height' => '70', 'class' => 'img-circle')) }}
 				@else
-					@if ( File::exists('assets/img/profile_images/' . User::find($conversation->owner)->picture ))
-						{{ HTML::image('assets/img/profile_images/'.User::find($conversation->owner)->picture, 'profile picture', array('width' => '70', 'height' => '70', 'class' => 'img-circle')) }}
-					@else
-						{{ HTML::image('assets/img/dummy.png', $user->id , array('width' => '70', 'height' => '70', 'class' => 'img-circle')) }}
-					@endif
+					<a href="{{URL::to('profile', $conversation->owner)}}">
+						{{HTML::image(User::find($conversation->owner)->getProfilePictureURL(), '$post->user->id', array('width' => '70', 'height' => '70', 'class' => 'img-circle'))}}
+					</a>
 				@endif 
 			</div>
-			<h2><a href="{{ URL::to('showConversation/'.$conversation->id) }}">{{{$conversation->name}}}</a></h2>
+			<h2 style="margin:0px; margin-bottom:3px"><a href="{{ URL::to('showConversation/'.$conversation->id) }}">{{{$conversation->name}}}</a></h2>
+			@foreach ($conversation->users()->get() as $conversation_member)
+				<a href="{{URL::to('profile', $conversation_member->id)}}">
+					{{HTML::image($conversation_member->getProfilePictureURL(), '$conversation_member->id', array('title' => $conversation_member->first, 'width' => '30', 'height' => '30', 'class' => 'img-circle'))}}
+				</a>
+			@endforeach
 			<a href="{{ URL::to('leaveConversation/'.$conversation->id) }}" style="text-decoration:none;float:right;margin-top:-16px;" onclick="return confirm('Are you sure you want to leave this conversation? You will not be able to view these messages or reply any longer.');"><span style="color:red;">Leave This Conversation</span></a>
 		</div>
 	@endforeach
